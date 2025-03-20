2a49a78a575334b9ccfd64b53687f0fbc3ac9bce
manen <49951816+manen@users.noreply.github.com>
2025-03-20 21:12:44 +0100

    equator: `Expr::simplify`

--- equator/src/lib.rs
+++ equator/src/lib.rs
@@ -27,8 +27,82 @@ impl Expr {
 	pub fn divide(a: Expr, b: Expr) -> Self {
 		Self::Divide(Box::new(a), Box::new(b))
 	}
+
+	pub fn calculate(&self) -> Option<f64> {
+		match self {
+			&Self::Constant(num) => Some(num),
+			Self::Add(a, b) => a
+				.calculate()
+				.map(|a| b.calculate().map(|b| a + b))
+				.flatten(),
+			Self::Subtract(a, b) => a
+				.calculate()
+				.map(|a| b.calculate().map(|b| a - b))
+				.flatten(),
+			Self::Multiply(a, b) => a
+				.calculate()
+				.map(|a| b.calculate().map(|b| a * b))
+				.flatten(),
+			Self::Divide(a, b) => a
+				.calculate()
+				.map(|a| b.calculate().map(|b| a / b))
+				.flatten(),
+			_ => None,
+		}
+	}
+
+	pub fn simplify(self) -> Self {
+		self.calculate()
+			.map(|num| Self::Constant(num))
+			.unwrap_or_else(|| match self {
+				Self::Add(a, b) => Self::add(a.simplify(), b.simplify()),
+				Self::Subtract(a, b) => Self::subtract(a.simplify(), b.simplify()),
+				Self::Multiply(a, b) => Self::multiply(a.simplify(), b.simplify()),
+				Self::Divide(a, b) => Self::divide(a.simplify(), b.simplify()),
+				_ => self,
+			})
+	}
 }
 
 // how do we describe an equation
 
 // expr = expr = expr
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_simplify_1() {
+		let expr = "4 * 3";
+		let expr = parse::parse(expr).expect("failed to parse");
+		let expr = expr.simplify();
+		if let Expr::Constant(num) = expr {
+			assert_eq!(num, 12.0);
+		} else {
+			panic!("should've been simplified");
+		}
+	}
+	#[test]
+	fn test_simplify_2() {
+		let expr = "4 * 3 / 2 * 10";
+		let expr = parse::parse(expr).expect("failed to parse");
+		let expr = expr.simplify();
+		if let Expr::Constant(num) = expr {
+			assert_eq!(num, 60.0);
+		} else {
+			panic!("should've been simplified");
+		}
+	}
+	#[test]
+	fn test_simplify_3() {
+		let expr = "4 * 3 / 2 * 10 * x";
+		let expr = parse::parse(expr).expect("failed to parse");
+		let expr = expr.simplify();
+		if let Expr::Constant(num) = expr {
+			assert_eq!(num, 60.0);
+		} else {
+			panic!("should've been simplified {expr:#?}");
+		}
+	}
+}
